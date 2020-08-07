46707b5184f8a6388a0e985be1929148a5130f8f
dscarmo <carmodiedre@outlook.com>
2020-08-07 14:03:10 -0300

    general upgrade

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request, send_file
 from werkzeug import secure_filename
+import sys
+sys.path.append("/home/diedre/git/radvid19")
+import subprocess
+import os
 
 from imageio import imwrite, imread
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/index')
 def upload_file():
-    return render_template('upload.html')
+    return render_template('index.html')
 
 
 @app.route('/uploaded', methods=['GET', 'POST'])
@@ -16,30 +20,27 @@ def upload_route():
     if request.method == 'POST':
         f = request.files['file']
         fname = secure_filename(f.filename)
-        f.save(fname)
-
-        render_template('loading.html')
+        saved_fname = os.path.join("inputs", fname)
+        f.save(saved_fname)
 
         # Process...
-        print('Processing...')
         try:
-            im = imread(fname)
-            im = (im - im.min()) / (im.max() - im.min())
+            print('Processing...')
+            im = imread(saved_fname)
+
+            # Return
+            imwrite(os.path.join("outputs", os.path.basename(saved_fname)), im)
+            print("Saved processed file.")
         except Exception as e:
             return str(e)
 
-        # Return
-        imwrite("processed.png", im)
-        print("Saved processed file.")
-
-        render_template('download.html')
+        return render_template('download.html')
 
 
 @app.route('/return_processed/')
 def return_processed():
     try:
         return send_file("processed.png", attachment_filename="processed.png")
-        render_template("upload.html")
     except Exception as e:
         return str(e)
 
